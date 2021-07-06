f492784fd1b0f398eab04b748a6892475bb14d2c
Kevin Scroggins <nitro404@gmail.com>
2021-07-06 11:01:13 -0400

    cURL Hunterization

---
+++ CMake/Hunter/HunterConfig.cmake
@@ -0,0 +1,11 @@
+include_guard()
+
+hunter_config(OpenSSL
+	VERSION "1.1.1o"
+)
+
+hunter_config(ZLIB
+	URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-1.2.13.1.zip"
+	VERSION "1.2.13.1"
+	SHA1 "1189eb8d6a0d8f5b2cd676f09661f4864cc85896"
+)
